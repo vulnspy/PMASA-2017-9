17fc01a8fe117595d7a3485002222d4cf977bb8e
Ambulong <zeng.ambulong@gmail.com>
2018-06-10 23:09:57 +0800

    qcloud

--- vsplatego.sh
+++ vsplatego.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -O /var/www/phpMyAdmin-4.7.6-english.tar.gz https://files.phpmyadmin.net/phpMyAdmin/4.7.6/phpMyAdmin-4.7.6-english.tar.gz && rm -rf /var/www/html/* &&  tar -zxf /var/www/phpMyAdmin-4.7.6-english.tar.gz -C /var/www/html && mv /var/www/html/phpMyAdmin-4.7.6-english/* /var/www/html/ && rm -rf /var/www/html/phpMyAdmin-4.7.6-english;
+wget -O /var/www/phpMyAdmin-4.7.6-english.tar.gz http://test-1251457471.cosgz.myqcloud.com/phpMyAdmin-4.7.6-english.tar.gz && rm -rf /var/www/html/* &&  tar -zxf /var/www/phpMyAdmin-4.7.6-english.tar.gz -C /var/www/html && mv /var/www/html/phpMyAdmin-4.7.6-english/* /var/www/html/ && rm -rf /var/www/html/phpMyAdmin-4.7.6-english;
 chmod -R 777 /var/www/html
