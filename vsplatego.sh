7109c24f8142d7f4852615e1028457d7170388b8
Ambulong <zeng.ambulong@gmail.com>
2018-10-16 00:30:19 +0800

    docker-compose

--- vsplatego.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysql -u root -ptoor < /var/www/html/db.sql
-wget -O /var/www/phpMyAdmin-4.7.6-english.tar.gz http://test-1251457471.cosgz.myqcloud.com/phpMyAdmin-4.7.6-english.tar.gz && rm -rf /var/www/html/* &&  tar -zxf /var/www/phpMyAdmin-4.7.6-english.tar.gz -C /var/www/html && mv /var/www/html/phpMyAdmin-4.7.6-english/* /var/www/html/ && rm -rf /var/www/html/phpMyAdmin-4.7.6-english;
-chmod -R 777 /var/www/html
